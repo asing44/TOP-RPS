93ad7c333e79a1b670b4389e226a082ab13b4a2f
asing44 <adamwarsing@gmail.com>
2024-01-12 17:41:16 -0500

    Creating UI

--- scripts.js
+++ scripts.js
@@ -2,35 +2,40 @@ let computerScore = 0;
 let playerScore = 0;
 let gamesPlayed = 0;
 
+const playerChoiceText = document.getElementsByClassName("player-choice-text")[0];
+playerChoiceText.textContent = "";
+
 function getChoices() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) {
-        return 'ROCK'
+        return 'Rock'
     } else if (computerChoice == 1) {
-        return 'PAPER'
+        return 'Paper'
     } else {
-        return 'SCISSORS'
+        return 'Scissors'
     }
 }
 
-function rps() {
-    let playerInput = prompt("What is your move? Rock, Paper, or Scissors?")
-    if (playerInput == 'ROCK') {
-        if (getChoices() == 'PAPER') {
+function rps(playerMove) {
+    if (playerMove == 'Rock') {
+        playerChoiceText.textContent = "Rock!";
+        if (getChoices() == 'Paper') {
             computerScore += 1;
-        } else if (getChoices() == 'SCISSORS') {
+        } else if (getChoices() == 'Scissors') {
             playerScore += 1;
         }
-    } else if (playerInput == "PAPER") {
-        if (getChoices() == "ROCK") {
+    } else if (playerMove == "Paper") {
+        playerChoiceText.textContent = "Paper!";
+        if (getChoices() == "Rock") {
             playerScore += 1;
-        } else if (getChoices() == "SCISSORS") {
+        } else if (getChoices() == "Scissors") {
             computerScore += 1;
         }
     } else {
-        if (getChoices() == "ROCK") {
+        playerChoiceText.textContent = "Scissors!";
+        if (getChoices() == "Rock") {
             computerScore += 1;
-        } else if (getChoices() == "PAPER") {
+        } else if (getChoices() == "Paper") {
             playerScore += 1;
         }
     }
